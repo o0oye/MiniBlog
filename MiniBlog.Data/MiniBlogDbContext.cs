132f0004a22cc2b3729c91e4c0c4c618b5ddfee4
xiaojvhaung <junkaih@qq.com>
2020-04-25 23:11:04 +0800

    仓储

---
+++ MiniBlog.Data/MiniBlogDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniBlog.Data
+{
+    public class MiniBlogDbContext: DbContext
+    {
+    }
+}
