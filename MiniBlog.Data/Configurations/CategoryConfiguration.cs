23eb29cd1c11eca6e9dfa22b9b04250ca1880dcb
xiaojvhaung <junkaih@qq.com>
2020-04-26 00:15:28 +0800

    配置数据映射类

---
+++ MiniBlog.Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MiniBlog.Data.Dto;
+
+namespace MiniBlog.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<CategoryDto>
+    {
+        public void Configure(EntityTypeBuilder<CategoryDto> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Category)
+                .HasMaxLength(64)
+                .IsRequired();
+        }
+    }
+}
---
+++ MiniBlog.Data/Configurations/PostConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MiniBlog.Data.Dto;
+
+
+namespace MiniBlog.Data.Configurations
+{
+    public class PostConfiguration : IEntityTypeConfiguration<PostDto>
+    {
+        public void Configure(EntityTypeBuilder<PostDto> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Title)
+                .HasMaxLength(64)
+                .IsRequired();
+            builder.Property(e => e.Summary)
+                .HasMaxLength(256);
+        }
+    }
+}
