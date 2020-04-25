62480212c6e2df865a93db405b6f67eb49f57852
xiaojvhaung <junkaih@qq.com>
2020-04-25 21:24:34 +0800

    项目架构

---
+++ MiniBlog.App.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MiniBlog.App.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
