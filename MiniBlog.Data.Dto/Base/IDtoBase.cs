a36e9ee0c175f92e3b0d6bfabb368a2b58ee215a
xiaojvhaung <junkaih@qq.com>
2020-04-25 21:50:42 +0800

    系统架构

--- MiniBlog.Data.Dto/Base/IDtoBase.cs
+++ MiniBlog.Data.Dto/Base/IDtoBase.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace MiniBlog.Data.Dto
+﻿namespace MiniBlog.Data.Dto
 {
     public interface IDtoBase<TPrimaryKey>
     {
