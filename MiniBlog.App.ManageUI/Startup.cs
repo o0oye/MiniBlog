ac25d879f6f4c1b57645d889a4fe076137aaa645
xiaojvhaung <junkaih@qq.com>
2020-04-26 01:44:57 +0800

    数据库

--- MiniBlog.App.ManageUI/Startup.cs
+++ MiniBlog.App.ManageUI/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MiniBlog.Core.Plugin.Injection;
 
 namespace MiniBlog.App.ManageUI
 {
@@ -20,13 +21,12 @@ namespace MiniBlog.App.ManageUI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMiniBlogMySqlDbContext(Configuration.GetConnectionString("MysqlConnection"));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +36,6 @@ namespace MiniBlog.App.ManageUI
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
---
+++ MiniBlog.Core.Plugin/Injection/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MiniBlog.Data;
+
+namespace MiniBlog.Core.Plugin.Injection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMiniBlogMySqlDbContext(this IServiceCollection services, string connection)
+        {
+            services.AddDbContext<MiniBlogDbContext>(options => options.UseMySql(connection));
+            return services;
+        }
+    }
+}
