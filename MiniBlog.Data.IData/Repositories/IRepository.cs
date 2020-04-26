353850fe352ef8ec3ad6864e55fbf1ca210efd38
xiaojvhaung <junkaih@qq.com>
2020-04-26 17:10:06 +0800

    仓储

---
+++ MiniBlog.Data.IData/Repositories/IRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MiniBlog.Data.Dto;
+
+namespace MiniBlog.Data.IData.Repositories
+{
+    public interface IRepository<TEntity, TPrimaryKey> where TEntity : class, IDtoBase<TPrimaryKey>
+    {
+        //返回可以远程查询
+        IQueryable<TEntity> GetQueryable();
+
+        //返回实体
+        ValueTask<TEntity> GetEntity(TPrimaryKey key);
+
+        //添加实体
+        ValueTask<EntityEntry<TEntity>> AddEntity(TEntity entity);
+
+        //批量添加实体
+        Task AddRange(IEnumerable<TEntity> entities);
+
+        //更新实体
+        EntityEntry<TEntity> UpdateEntity(TEntity entity);
+
+        //批量更新实体
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        //删除实体
+        EntityEntry<TEntity> RemoveEntity(TEntity entity);
+
+        //批量删除实体
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
---
+++ MiniBlog.Data/Repositories/Repository.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using MiniBlog.Data.Dto;
+using MiniBlog.Data.IData.Repositories;
+
+namespace MiniBlog.Data.Repositories
+{
+    public class Repository<TEntity, TPrimaryKey> : IRepository<TEntity, TPrimaryKey>
+        where TEntity : class, IDtoBase<TPrimaryKey>
+    {
+        private readonly MiniBlogDbContext _miniBlogDbContext;
+        private DbSet<TEntity> _dbSet => _miniBlogDbContext.Set<TEntity>();
+        public Repository(MiniBlogDbContext miniBlogDbContext)
+        {
+            _miniBlogDbContext = miniBlogDbContext;
+        }
+
+        //返回可以远程查询
+        public virtual IQueryable<TEntity> GetQueryable()
+        {
+            return _dbSet.AsQueryable();
+        }
+
+        //返回实体
+        public virtual async ValueTask<TEntity> GetEntity(TPrimaryKey key)
+        {
+            return await _dbSet.FindAsync(key);
+        }
+
+        //添加实体
+        public virtual async ValueTask<EntityEntry<TEntity>> AddEntity(TEntity entity)
+        {
+            return await _dbSet.AddAsync(entity);
+        }
+
+        //批量添加实体
+        public virtual async Task AddRange(IEnumerable<TEntity> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+        }
+
+        //更新实体
+        public virtual EntityEntry<TEntity> UpdateEntity(TEntity entity)
+        {
+            return _dbSet.Update(entity);
+        }
+
+        //批量更新实体
+        public virtual void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.UpdateRange(entities);
+        }
+
+        //删除实体
+        public EntityEntry<TEntity> RemoveEntity(TEntity entity)
+        {
+            return _dbSet.Remove(entity);
+        }
+
+        //批量删除实体
+        public void RemoveRange(IEnumerable<TEntity> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
