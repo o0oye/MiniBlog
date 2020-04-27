1743e723d0fd2400d1a9965bb34c4dd7539a2f80
xiaojvhaung <junkaih@qq.com>
2020-04-28 04:09:28 +0800

    AutoMapper

---
+++ MiniBlog.Core.Mapper/AdminProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MiniBlog.Data.Entity;
+using MiniBlog.Core.ViewModels.PostView;
+
+namespace MiniBlog.Core.Mapper
+{
+    public class AdminProfile: Profile
+    {
+        public AdminProfile()
+        {
+            CreateMap<AdminEntity, EditAdminViewModel>();
+        }
+    }
+}
